7b32c68dd72d7ca79eb53c38252483eaf941da4c
xs-han <hanxushen1105@sina.com>
2017-12-26 19:22:23 +0800

    estimator

--- get_started/mnist_beginner.py
+++ get_started/mnist_beginner.py
@@ -3,7 +3,7 @@ import numpy as np
 
 if __name__=='__main__':
     from tensorflow.examples.tutorials.mnist import input_data
-    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    mnist = input_data.read_data_sets("tmp/tensorflow/mnist/input_data", one_hot=True)
 
     x = tf.placeholder(tf.float32, [None, 784])
     W = tf.Variable(tf.zeros([784, 10]))
--- get_started/mnist_experts.py
+++ get_started/mnist_experts.py
@@ -90,7 +90,7 @@ def cnn_model(x,y_):
 if __name__ == '__main__':
     from tensorflow.examples.tutorials.mnist import input_data
 
-    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    mnist = input_data.read_data_sets('tmp/tensorflow/mnist/input_data', one_hot=True)
     sess = tf.InteractiveSession()
 
     x = tf.placeholder(tf.float32, shape=[None, 784])
