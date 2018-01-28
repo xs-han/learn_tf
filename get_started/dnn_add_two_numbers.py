3afecd38ea6da54846175a35aa33502864816091
xs-han <hanxushen1105@sina.com>
2018-01-28 11:28:23 +0800

    first version of dnn add two number

--- get_started/dnn_add_two_numbers.py
+++ get_started/dnn_add_two_numbers.py
@@ -9,6 +9,7 @@ NUM_DATA = 10000
 NUM_TRAINING_DATA = 7000
 NUM_VALIDATION_DATA = 2000
 NUM_TEST_DATA = 1000
+BATCH_SIZE = 50
 
 if __name__=='__main__':
 
@@ -28,25 +29,39 @@ if __name__=='__main__':
     x = tf.placeholder(tf.float32, shape=[None, 2])
     y_ = tf.placeholder(tf.float32, shape=[None, 1])
 
-    W1 = tf.Variable(tf.zeros([2, 10]))
+    W1 = tf.Variable(tf.truncated_normal([2, 10], stddev=0.1))
     b1 = tf.Variable(tf.zeros([10]))
 
-    W2 = tf.Variable(tf.zeros([10, 20]))
+    W2 = tf.Variable(tf.truncated_normal([10, 20], stddev=0.1))
     b2 = tf.Variable(tf.zeros([20]))
 
-    W3 = tf.Variable(tf.zeros([20, 1]))
+    W3 = tf.Variable(tf.truncated_normal([20, 1], stddev=0.1))
     b3 = tf.Variable(tf.zeros([1]))
 
-    a1 = tf.nn.sigmoid(tf.matmul(W1,x) + b1)
-    a2 = tf.nn.sigmoid(tf.matmul(W2,a1) + b2)
-    y = tf.nn.sigmoid(tf.matmul(W3,a2) + b3)
+    sess = tf.InteractiveSession()
+    sess.run(tf.global_variables_initializer())
 
-    coss = tf.reduce_mean((y - y_) * (y - y_))
+    a1 = tf.nn.relu6(tf.matmul(x, W1) + b1)
+    a2 = tf.nn.relu6(tf.matmul(a1,W2) + b2)
+    y = tf.matmul(a2,W3) + b3
 
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(coss)
+    coss = tf.reduce_mean((tf.nn.l2_loss(y-y_)))
 
-    for i in range(1000):
-        batch = training_set[i*10:(i+1)*10,:]
-        train_step.run(feed_dict={x: batch[0], y_: training_ans[i*10:(i+1)*10]})
+    train_step = tf.train.GradientDescentOptimizer(0.005).minimize(coss)
 
-    print(coss.eval(feed_dict={x: test_set, y_: test_ans}))
+    for epoch in range(100):
+        train_index = np.arange(1, NUM_TRAINING_DATA)
+        np.random.shuffle(train_index)
+        run_training_set = training_set[train_index,:]
+        run_train_ans = training_ans[train_index,:]
+        for i in range(int(NUM_TRAINING_DATA/BATCH_SIZE)):
+            train_step.run(feed_dict={x: run_training_set[i*BATCH_SIZE:(i+1)*BATCH_SIZE,:],
+                                      y_: run_train_ans[i*BATCH_SIZE:(i+1)*BATCH_SIZE,:]})
+
+    print('This is a toy example of floating adding using DNN')
+    print('Average loss on test set: ', coss.eval(feed_dict={x: test_set, y_: test_ans}))
+    test_by_hand = np.array([[0.2,0.6],[0.1,0.3]], dtype=np.float32)
+    test_by_hand.shape=(2,2)
+    test_by_hand_ans = np.array([0, 0], dtype=np.float32)
+    test_by_hand_ans.shape = (2, 1)
+    print('Manual test at: \n', test_by_hand, '\nResult: \n', sess.run(y, feed_dict={x:test_by_hand, y_: test_by_hand_ans}))
